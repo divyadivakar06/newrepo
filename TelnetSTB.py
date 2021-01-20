e47c2505bdd5befffefc33884c031615203bfe24
Divakar Divya <divya.divakar@nagra.com>
2021-01-20 12:49:21 +0530

    changes for telnet connection!

---
+++ TelnetSTB.py
@@ -0,0 +1,16 @@
+import base64
+import telnetlib
+
+hostip = "192.168.1.1"
+user = "root"
+
+session = telnetlib.Telnet(hsotip)
+session.read_until(b"login: ")
+session.write(user.encode('ascii')+b"\n")
+
+f = open("test2.py")
+a=f.read()
+encodedBytes = base64.b64encode(a.encode("utf-8"))
+encodedStr = str(encodedBytes, "utf-8")
+
+print(encodedStr)
